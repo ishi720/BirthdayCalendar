a3245418f807500c08ec95c383a2b3d12dc6ee77
ishi720 <ishizuka720@gmail.com>
2024-05-12 19:48:52 +0900

    キャラクターの追加ページと一覧画面

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,32 @@
 class CharactersController < ApplicationController
     def index
+        @characters = Character.all
+    end
+
+    def new
+        @character = Character.new
+    end
+
+    def create
+        @character = Character.new(character_params)
+
+        if @character.save
+            redirect_to characters_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def character_params
+        params.require(:character).permit(
+            :name,
+            :nickname,
+            :birthday,
+            :tag,
+            :icon_image,
+            :description
+        )
     end
 end

ce9ffeda8ef958154a18b3af92e9090cf6e0c251
ishi720 <ishizuka720@gmail.com>
2024-05-22 23:13:14 +0900

    カレンダーの一番左を日曜日に変更 #11

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,5 +1,12 @@
 class CalendarController < ApplicationController
+  before_action :set_beginning_of_week
+
   def index
     @characters = Character.all
+  end
+
+  private
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
   end
 end

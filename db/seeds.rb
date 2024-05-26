c0c03efd7a9e492b53da33bf525967da2ce10e3c
ishi720 <ishizuka720@gmail.com>
2024-05-27 00:49:06 +0900

    seedでデータを用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Character.create([
+  {
+    name: 'ポムポムプリン',
+    nickname: 'プリン',
+    tag: 'サンリオ',
+    icon_image: '',
+    description: '',
+    month: 4,
+    day: 16
+  },
+  {
+    name: 'シナモロール',
+    nickname: 'シナモン',
+    tag: 'サンリオ',
+    icon_image: '',
+    description: '',
+    month: 3,
+    day: 6
+  },
+  {
+    name: 'モンキー・D・ルフィ',
+    nickname: 'ルフィ',
+    tag: 'ONE PIECE',
+    icon_image: '',
+    description: '',
+    month: 5,
+    day: 5
+  },
+  {
+    name: 'シャンクス',
+    nickname: 'シャンクス',
+    tag: 'ONE PIECE',
+    icon_image: '',
+    description: '',
+    month: 3,
+    day: 9
+  },
+  {
+    name: '江戸川コナン',
+    nickname: 'コナン',
+    tag: '名探偵コナン',
+    icon_image: '',
+    description: '',
+    month: 5,
+    day: 4
+  }
+])
